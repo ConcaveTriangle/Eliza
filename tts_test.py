a4b2720f059829b600850461206646dabb17c162
ConcaveTriangle <jerryxguo2006@gmail.com>
2024-05-17 12:17:32 -0700

    Changing streaming structure

--- tts_test.py
+++ tts_test.py
@@ -1,47 +1,25 @@
-from flask import Flask, request, jsonify, send_file
-from hashlib import sha256
-import base64
-import numpy
+import os
 import torch
 import torchaudio
-from transformers import AutoProcessor, AutoModelForSpeechSeq2Seq, pipeline
-from llama_cpp import Llama
-import wave
-import pyaudio
-import time
-import wave
-from TTS.api import TTS
-import pyaudio
+from TTS.tts.configs.xtts_config import XttsConfig
+from TTS.tts.models.xtts import Xtts
 
-# Text-to-Speech streaming setup
-print("Loading TTS model...")
-print(TTS().list_models())
-tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2", gpu=True).to("cuda")
+print("Loading model...")
+config = XttsConfig()
+config.load_json("./models/XTTS-v2/config.json")
+model = Xtts.init_from_config(config)
+model.load_checkpoint(config, checkpoint_dir="./models/XTTS-v2/", use_deepspeed=True)
+model.cuda()
 
 print("Computing speaker latents...")
-gpt_cond_latent, speaker_embedding = tts.get_conditioning_latents(audio_path=["reference.wav"])
+gpt_cond_latent, speaker_embedding = model.get_conditioning_latents(audio_path=["./audio_samples/Dolly-Recording-1.wav", "./audio_samples/Dolly-Recording-2.wav", "./audio_samples/Dolly-Recording-4.wav", "./audio_samples/Dolly-Recording-5.wav", "./audio_samples/Dolly-Recording-6.wav", "./audio_samples/Dolly-Recording-7.wav"])
 
-def text_to_speech_streaming(text):
-    print("Starting TTS streaming...")
-    t0 = time.time()
-    chunks = tts.inference_stream(
-        text,
-        "en",
-        gpt_cond_latent,
-        speaker_embedding
-    )
-
-    wav_chunks = []
-    for i, chunk in enumerate(chunks):
-        if i == 0:
-            print(f"Time to first chunk: {time.time() - t0}")
-        print(f"Received chunk {i} of audio length {chunk.shape[-1]}")
-        wav_chunks.append(chunk)
-
-    wav = torch.cat(wav_chunks, dim=0)
-    output_path = "./ai_output.wav"
-    torchaudio.save(output_path, wav.squeeze().unsqueeze(0).cpu(), 24000)
-    return output_path
-
-while True:
-    text_to_speech_streaming(input("Testing: "))
+print("Inference...")
+out = model.inference(
+    "It took me quite a long time to develop a voice and now that I have it I am not going to be silent.",
+    "en",
+    gpt_cond_latent,
+    speaker_embedding,
+    temperature=0.7, # Add custom parameters here
+)
+torchaudio.save("ai_output.wav", torch.tensor(out["wav"]).unsqueeze(0), 24000)
