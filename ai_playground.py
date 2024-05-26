2bcde398517172ea251db1ee9307636d176c1d12
ConcaveTriangle <jerryxguo2006@gmail.com>
2024-05-26 00:52:15 -0700

    Used noisereduce to process samples for finetuning

--- ai_playground.py
+++ ai_playground.py
@@ -1,8 +1,14 @@
 from scipy.io import wavfile
 import noisereduce
+import numpy as np
 
-rate, data = wavfile.read("./audio_samples/mary/Mary-Recording-41.wav")
-print("Original rate:", rate)
+for x in range(77):
+    x += 1
+    path = "./mary/Mary-Recording-" + str(x) + ".wav"
+    print("Starting: " + path)
+    rate, data = wavfile.read(path)
+    original_shape = data.shape
+    data = np.reshape(data, (2, -1))
 
-reduce_noise = noisereduce.reduce_noise(y=data, sr=rate)
-wavfile.write("reduced_noise.wav", rate, reduce_noise)
+    reduce_noise = noisereduce.reduce_noise(y=data, sr=rate, prop_decrease=0.95)
+    wavfile.write("testing-" + str(x) + ".wav" , rate, reduce_noise.reshape(original_shape))
